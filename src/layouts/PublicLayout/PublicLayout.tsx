7fd44368772f7bd9b22b537c38aa4da38f5b016c
Leo Heng <leoheng905@gmail.com>
2024-03-02 08:14:31 +1100

    Added google login

--- src/layouts/PublicLayout/PublicLayout.tsx
+++ src/layouts/PublicLayout/PublicLayout.tsx
@@ -1,12 +1,16 @@
 import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 const PublicLayout = () => {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Outlet />
-    </div>
+    <GoogleOAuthProvider clientId="1013964470-v6pc6mdo4f0ctek97kimggbpt04q04ft.apps.googleusercontent.com">
+      <div>
+        <Navbar></Navbar>
+        <Outlet />
+      </div>
+    </GoogleOAuthProvider>
   );
 };
 
---
+++ src/routes/signIn/SignIn.tsx
@@ -0,0 +1,29 @@
+import { GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const SignIn = () => {
+  return (
+    <GoogleOAuthProvider clientId="1013964470-v6pc6mdo4f0ctek97kimggbpt04q04ft.apps.googleusercontent.com">
+      <div className="flex h-screen w-screen max-w-full flex-row">
+        <div className=" h-full w-3/5 p-16">
+          <img className="h-full w-full rounded"></img>
+        </div>
+        <div className="flex h-full w-2/5 items-center">
+          <div className="flex flex-col gap-4 rounded bg-neutral-50 p-8">
+            <img className=" h-80 w-80"></img>
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                console.log(credentialResponse);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+
+export default SignIn;
