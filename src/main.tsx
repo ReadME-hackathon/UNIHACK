4bb1321554724aa06d51a0e0e3e89b95911e3a0c
vjy02 <vyojoh@gmail.com>
2024-03-02 13:12:56 +1100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -15,12 +15,17 @@ import LandingPage from "./routes/landing/LandingPage";
 import PublicLayout from "./layouts/PublicLayout/PublicLayout";
 import SignIn from "./routes/signIn/SignIn";
 
+import JoinRoom from "./routes/onboardingPages/onboardingStudent/JoinRoom";
+import ProfileSetup from "./routes/onboardingPages/onboardingStudent/ProfileSetup";
+
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route path="/" element={<PublicLayout />}>
       <Route index element={<LandingPage />} />
       <Route path="createPlatform" element={<LandingPage />} />
       <Route path="joinPlatform" element={<LandingPage />} />
+      <Route path="1" element={<JoinRoom  />} />
+      <Route path="2" element={<ProfileSetup />} />
     </Route>,
     <Route path="signIn" element={<SignIn />} />,
   ]),
