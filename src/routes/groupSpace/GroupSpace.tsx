851a502db10ec2c50959b042c11ab053e84775b5
Leo Heng <leoheng905@gmail.com>
2024-03-02 23:48:25 +1100

    Added members list generation

--- src/routes/groupSpace/GroupSpace.tsx
+++ src/routes/groupSpace/GroupSpace.tsx
@@ -1,15 +1,13 @@
 import MembersList from "@/components/MembersList/MembersList";
 import TutorialCard from "@/components/TutorialCard/TutorialCard";
-import TutorialCardProps from "@/components/TutorialCard/TutorialCardProps";
 import { getSpaceData } from "@/services/spacesServices";
 
-import { Space, Group } from "@/services/models";
+import { Space } from "@/services/models";
 
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const GroupSpace = () => {
-  const [groupList, setGroupList] = useState<Group[]>([]);
   const [spaceData, setSpaceData] = useState<Space>();
 
   // Gets the space ID
@@ -21,14 +19,10 @@ const GroupSpace = () => {
       spaceId = "";
     }
 
-    console.log("good luck!");
     getSpaceData(spaceId)
       .then((result) => {
         console.log(result);
         setSpaceData(result?.data as Space);
-        if (!spaceData) {
-          setGroupList(spaceData!.subcollections.groups);
-        }
       })
       .catch((error: any) => {
         console.log(error);
@@ -39,18 +33,19 @@ const GroupSpace = () => {
     <div className="flex w-full flex-row p-8">
       <div className="w-2/3 pr-8">
         <div className="3xl:grid-cols-4 grid grid-flow-row grid-cols-2 gap-4 2xl:grid-cols-3 ">
-          {groupList.map((team: any) => (
+          {spaceData?.subcollections.groups.map((team: any, index) => (
             <TutorialCard
               name={team.name}
               enrolled={team.member_count}
               size={spaceData?.space.max_size || 0}
               description={team.description}
+              key={index}
             />
           ))}
         </div>
       </div>
       <div className="w-1/3">
-        <MembersList spaceId={spaceId}></MembersList>
+        <MembersList spaceUser={spaceData?.subcollections.users!}></MembersList>
       </div>
     </div>
   );
