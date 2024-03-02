9e132097adde344b19b48264bf2cee1b70a911da
Leo Heng <leoheng905@gmail.com>
2024-03-03 00:00:12 +1100

    Restyled tutorial cards as avatars are not working

--- src/components/TutorialCard/TutorialCard.tsx
+++ src/components/TutorialCard/TutorialCard.tsx
@@ -2,17 +2,19 @@ import TutorialCardProps from "./TutorialCardProps";
 
 const TutorialCard = ({ name, size, enrolled, description }: TutorialCardProps) => {
   return (
-    <div className="flex h-56 w-full flex-col rounded-[6px] bg-orange-100 pl-3">
-      <h3 className="mb-4 text-2xl font-bold">{name}</h3>
+    <div className="flex h-56 w-full flex-col rounded-[6px] bg-orange-100 p-8">
+      <h3 className="text-2xl font-bold">{name}</h3>
+      <p className="text-sm font-medium">
+        Size: {enrolled}/{size}
+      </p>
       {/* <div className="mb-20 flex">
         {avatars.map((avatar) => (
           <img src={avatar} className="h-6 w-6 rounded-xl" />
         ))}
       </div> */}
-      <p className="text-sm font-medium">
-        Size: {enrolled}/{size}
-      </p>
-      <p className="text-sm font-medium">{description}</p>
+      <div className="mt-auto">
+        <p className="text-sm font-medium">{description}</p>
+      </div>
     </div>
   );
 };
