feda87166dd1d676b310938ad257d693c09b6ef8
alie-nel <msnguyen63@gmail.com>
2024-03-02 13:00:52 +1100

    make icons smaller

--- src/components/Landing/LandingBar.tsx
+++ src/components/Landing/LandingBar.tsx
@@ -4,9 +4,9 @@ const LandingBar = () => {
       <div className="flex h-10 items-center w-[650px] pl-4 mr-4 rounded bg-white">
         <h4 className="text-sm font-medium">Small title</h4>
       </div>
-      <div className="w-6 h-6 rounded-xl bg-slate-500 "></div>
-      <div className="w-6 h-6 rounded-xl bg-yellow-400 "></div>
-      <div className="w-6 h-6 rounded-xl bg-red-500"></div>
+      <div className="w-5 h-5 rounded-xl bg-slate-500 "></div>
+      <div className="w-5 h-5 rounded-xl bg-yellow-400 "></div>
+      <div className="w-5 h-5 rounded-xl bg-red-500"></div>
     </div>
   );
 };
