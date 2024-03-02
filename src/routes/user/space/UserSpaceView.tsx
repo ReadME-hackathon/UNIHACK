da7a79b9d67214a24c6aab27562110c67eecef2b
Leo Heng <leoheng905@gmail.com>
2024-03-03 10:03:19 +1100

    Added no group handling

--- src/routes/user/space/UserSpaceView.tsx
+++ src/routes/user/space/UserSpaceView.tsx
@@ -33,15 +33,17 @@ const UserSpaceView = () => {
     <div className="flex w-full flex-row p-8">
       <div className="w-2/3 pr-8">
         <div className="3xl:grid-cols-4 grid grid-flow-row grid-cols-2 gap-4 2xl:grid-cols-3 ">
-          {spaceData?.subcollections.groups.map((team: any, index) => (
-            <TutorialCard
-              name={team.name}
-              enrolled={team.member_count}
-              size={spaceData?.space.max_size || 0}
-              description={team.description}
-              key={index}
-            />
-          ))}
+          {spaceData?.subcollections.groups
+            ? spaceData?.subcollections.groups.map((team: any, index) => (
+                <TutorialCard
+                  name={team.name}
+                  enrolled={team.member_count}
+                  size={spaceData?.space.max_size || 0}
+                  description={team.description}
+                  key={index}
+                />
+              ))
+            : "No groups yet"}
         </div>
       </div>
       <div className="w-1/3">
