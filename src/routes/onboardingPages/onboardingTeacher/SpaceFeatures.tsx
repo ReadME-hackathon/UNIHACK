21b8ddd80f5b65ef0789a366345b115e7cb10a92
vjy02 <vyojoh@gmail.com>
2024-03-02 13:09:55 +1100

    Added updated teacher onboarding UI design

--- src/routes/onboardingPages/onboardingTeacher/SpaceFeatures.tsx
+++ src/routes/onboardingPages/onboardingTeacher/SpaceFeatures.tsx
@@ -1,13 +1,17 @@
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
-  
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Button } from "@/components/ui/button"
+
+interface SwitchFeatures {
+    id: string,
+    name: string,
+}
+
+interface SwitchComponentProps {
+    switches: SwitchFeatures[];
+}
+
 function CreateSpace() {
     const defaultFeatures = {
         "Music":{
@@ -33,34 +37,29 @@ function CreateSpace() {
     }
     // If "Purpose" was selected, get default values from defaultFeatures
 
-    const TableComponent = () => { 
+    const SwitchComponent = ({ switches }: SwitchComponentProps) => {
         return (
-            <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                    <TableHead className="w-[100px]">Invoice</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Method</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                    <TableCell className="font-medium">INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
-                    </TableRow>
-                </TableBody>
-                </Table>
-        )
+            <ScrollArea className="flex-col h-1/3 w-10/12 ounded-md border p-4 overflow-y-auto">
+                {switches.map((curSwitch: SwitchFeatures) => (
+                    <div className="flex items-center mt-4">
+                        <Switch id={curSwitch.id} className="bg-black mr-5"/>
+                        <Label htmlFor={curSwitch.id}>{curSwitch.name}</Label>
+                    </div>
+                ))}
+                <Button className="bg-orange-100 mt-6 w-5/6">Create new criteria</Button>
+            </ScrollArea>
+        );
     }
+
   return (
     <div className="mx-auto flex h-screen items-center justify-center">
         <div className="flex h-4/6 w-1/2 flex-col justify-center gap-10">
           <h2 className="text-6xl font-bold leading-snug">Allow matching<br/>by features</h2>
-          <TableComponent />
+          <SwitchComponent switches={[{id:"test",name:"test"},{id:"test",name:"test"},{id:"test",name:"test"},{id:"test",name:"test"},{id:"test",name:"test"},{id:"test",name:"test"}]}/>
+          <div className="flex gap-5">
+            <Button className="rounded bg-zinc-300 text-white">Back</Button>
+            <Button className="rounded bg-orange-500 text-white">Continue</Button>
+          </div>
         </div>
       </div>
   );
