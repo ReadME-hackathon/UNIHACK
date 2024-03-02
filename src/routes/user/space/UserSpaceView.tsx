cce12013e357ccd9adc013354fe33c178dcfaed1
vjy02 <vyojoh@gmail.com>
2024-03-03 10:39:41 +1100

    Added skeleton UI for space view

--- src/routes/user/space/UserSpaceView.tsx
+++ src/routes/user/space/UserSpaceView.tsx
@@ -6,9 +6,17 @@ import { Space } from "@/services/models";
 
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Link2Icon } from "@radix-ui/react-icons";
 
 const UserSpaceView = () => {
   const [spaceData, setSpaceData] = useState<Space>();
+  const [displayPopup, setPopup] = useState<boolean>(false)
+  const [spaceName, setSpaceName] = useState("");
+
+  const togglePopup = () => {
+    setPopup(!displayPopup);
+  };
 
   // Gets the space ID
   let { space_id } = useParams();
@@ -22,6 +30,7 @@ const UserSpaceView = () => {
     getSpaceData(space_id)
       .then((result) => {
         console.log(result);
+        setSpaceName((result?.data as { space: { name: string } }).space.name);
         setSpaceData(result?.data as Space);
       })
       .catch((error: any) => {
@@ -30,9 +39,18 @@ const UserSpaceView = () => {
   }, []);
 
   return (
-    <div className="flex w-full flex-row p-8">
+    <>
+    <div className={`flex w-full flex-row h-screen px-8 py-3 ${displayPopup ? "opacity-30" : ""}`}>
       <div className="w-2/3 pr-8">
-        <div className="3xl:grid-cols-4 grid grid-flow-row grid-cols-2 gap-4 2xl:grid-cols-3 ">
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-bold">{`Code: ${space_id}`}</h3>
+          <h1 className="text-6xl font-bold">{spaceName}</h1>
+          <div className="flex items-center gap-7 my-6">
+            <h2 className="text-3xl font-bold ">Groups</h2>
+            <Button onClick={togglePopup} className="font-bold bg-neutral-200 hover:bg-neutral-200 rounded">Create Group</Button>
+          </div>
+        </div>
+        <div className="3xl:grid-cols-4 grid grid-flow-row grid-cols-2 gap-4 2xl:grid-cols-3 overflow-y-auto">
           {spaceData?.subcollections.groups
             ? spaceData?.subcollections.groups.map((team: any, index) => (
                 <TutorialCard
@@ -43,14 +61,32 @@ const UserSpaceView = () => {
                   key={index}
                 />
               ))
-            : "No groups yet"}
+            : <></>}
+           <a onClick={togglePopup} className="flex h-56 w-full hover:cursor-pointer flex-col rounded-[6px] bg-neutral-100 justify-center items-center text-center text-3xl leading-relaxed font-bold">
+              + <br/>
+              Create <br/>
+              Group
+           </a>
         </div>
       </div>
-      <div className="w-1/3">
-        <MembersList spaceUser={spaceData?.subcollections.users!}></MembersList>
+      <div className="w-1/3 flex flex-col justify-between gap-10 max-h-[950px] min-h-[700px]">
+        <div className="h-1/6 bg-neutral-100 mb-16 font-bold underline flex gap-5 items-center justify-center text-3xl"><Link2Icon className="scale-150"/>Share Link</div>
       </div>
     </div>
+    {displayPopup ? (
+        <div>popup
+        {/*<NewGroupPopup onClose={togglePopup} onSubmit={()=>{}}/>*/}</div>
+      ): (<></>)}
+    </>
   );
 };
 
 export default UserSpaceView;
+
+
+
+
+
+
+
+
