11727420c8ca93ba28e722dd8065bc14883fd931
alie-nel <msnguyen63@gmail.com>
2024-03-02 11:48:38 +1100

    add title and buttons to hero

--- src/components/Landing/LandingFeature.tsx
+++ src/components/Landing/LandingFeature.tsx
@@ -2,7 +2,7 @@ import LandingFeatureProps from "./LandingFeatureProps";
 
 const LandingFeature = ({ name, desc, img } : LandingFeatureProps) => {
   return (
-    <div className="flex flex-row gap-36 items-center w-[900px] m-auto">
+    <div className="flex flex-row gap-36 items-center w-[800px] m-auto">
       <div className="flex flex-col gap-6">
         <h3 className="text-4xl font-bold">{name}</h3>
         <p className="h-24 font-medium">{desc}</p>
