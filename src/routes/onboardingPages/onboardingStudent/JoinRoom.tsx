e05734c1cbb47ec4a3f418809374606c04edc5b1
vjy02 <vyojoh@gmail.com>
2024-03-02 12:16:41 +1100

    Updated components to fit new design

--- src/routes/onboardingPages/onboardingStudent/JoinRoom.tsx
+++ src/routes/onboardingPages/onboardingStudent/JoinRoom.tsx
@@ -1,7 +1,7 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -9,63 +9,68 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useNavigate } from 'react-router-dom';
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useNavigate } from "react-router-dom";
 
 function JoinRoom() {
-
-  const FormComponent = () =>{
-
+  const FormComponent = () => {
     const formSchema = z.object({
       roomCode: z.string().min(4, {
         message: "Invalid room code number",
       }),
-    })
+    });
 
     const form = useForm<z.infer<typeof formSchema>>({
       resolver: zodResolver(formSchema),
       defaultValues: {
         roomCode: "",
       },
-    })
+    });
     const navigate = useNavigate();
     // SUBMIT HANDLER
     function onSubmit(values: z.infer<typeof formSchema>) {
-      console.log(values)
-      navigate("/profileSetup")
+      console.log(values);
+      navigate("/profileSetup");
       // Handle room number authentication
-
     }
 
-    return(
+    return (
       <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="">
-        <FormField
-          control={form.control}
-          name="roomCode"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Room code</FormLabel>
-              <FormControl>
-                <Input className="bg-gray-200 border-none w-5/6" placeholder="" {...field} />
-              </FormControl>
-              <FormMessage className="absolute" />
-            </FormItem>
-          )}
-        />
-        <Button className="mt-16 rounded text-white bg-indigo-500 hover:bg-indigo-500" type="submit">Continue</Button>
-      </form>
-    </Form>
-    )
-  }
+        <form onSubmit={form.handleSubmit(onSubmit)} className="">
+          <FormField
+            control={form.control}
+            name="roomCode"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Room code</FormLabel>
+                <FormControl>
+                  <Input className="w-5/6 border-none bg-indigo-100" placeholder="" {...field} />
+                </FormControl>
+                <FormMessage className="absolute" />
+              </FormItem>
+            )}
+          />
+          <Button
+            className="mt-16 rounded bg-indigo-500 text-white hover:bg-indigo-500"
+            type="submit"
+          >
+            Continue
+          </Button>
+        </form>
+      </Form>
+    );
+  };
 
   return (
-    <div className="max-w-screen-lg h-screen flex justify-between items-center m-auto">
-      <div className="h-4/6 w-2/5 bg-gray-200"></div>
-      <div className="h-4/6 w-3/6 font-bold text-6xl flex flex-col justify-center leading-snug">
-          <h2>Join a team<br/>finding a space</h2>
-          <FormComponent />
+    <div className="mx-auto flex h-screen items-center justify-center">
+      <div className="flex h-4/6 w-1/2 flex-col justify-center gap-10">
+        <h2 className="text-6xl font-bold leading-snug">
+          Join a team
+          <br />
+          finding a space
+        </h2>
+        <FormComponent />
       </div>
     </div>
   );
