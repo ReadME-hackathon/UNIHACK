a76c769576124bfeac746fcaf0182e03473e81ee
vjy02 <vyojoh@gmail.com>
2024-03-02 23:33:21 +1100

    Adjusted routing/file routing for user/admin

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,11 @@ import SignIn from "./routes/signIn/SignIn";
 import UserLayout from "./layouts/UserLayout/UserLayout";
 import DashboardHome from "./routes/dashboardHome/DashboardHome";
 import Layout from "./layouts/Layout";
-import GroupSpace from "./routes/groupSpace/GroupSpace";
+
+import AdminSpaceView from "./routes/admin/space/AdminSpaceView";
+import UserSpaceView from "./routes/user/space/UserSpaceView";
+import AdminMySpaces from "./routes/admin/mySpaces/AdminMySpaces";
+import UserMySpaces from "./routes/user/mySpaces/UserMySpaces";
 
 import JoinRoom from "./routes/onboardingPages/onboardingStudent/JoinRoom";
 import ProfileSetup from "./routes/onboardingPages/onboardingStudent/ProfileSetup";
@@ -34,19 +38,29 @@ const router = createBrowserRouter(
         <Route index element={<LandingPage />} />
         <Route path="createPlatform" element={<LandingPage />} />
         <Route path="joinPlatform" element={<LandingPage />} />
-        <Route path="group-space" element={<GroupSpace />} />
         <Route path="loading-team" element={<LoadingTeamPage />} />
       </Route>
       <Route path="signIn" element={<SignIn />} />
+
       <Route path="user" element={<UserLayout />}>
         <Route index element={<DashboardHome />} />
+        <Route path="space/*">
+          <Route index element={<UserMySpaces />} />
+          <Route path=":spaceId" element={<UserSpaceView/>} />
+        </Route>
         <Route path="join-room" element={<JoinRoom />} />
-        <Route path="create-space" element={<CreateSpace />} />
         <Route path="profile-setup" element={<ProfileSetup />} />
-        <Route path="edit-space-features" element={<SpaceFeatures />} />
       </Route>
-      <Route path="space" element={<UserLayout />}>
-        <Route path=":spaceId" element={<GroupSpace />} />
+
+      <Route path="admin" element={<UserLayout />}>
+        <Route index element={<DashboardHome />} />
+        <Route path="space/*">
+          <Route index element={<AdminMySpaces />} />
+          <Route path=":spaceId" element={<AdminSpaceView />} />
+        </Route>
+        <Route path="create-space" element={<CreateSpace />} />
+        <Route path="edit-space-features" element={<SpaceFeatures />} />
+        <Route path="profile-setup" element={<ProfileSetup />} />
       </Route>
     </Route>,
   ]),
@@ -54,6 +68,6 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <RouterProvider router={router} /> 
+  </React.StrictMode>,       
 );
---
+++ src/routes/admin/mySpaces/AdminMySpaces.tsx
@@ -0,0 +1,5 @@
+const AdminMySpaces = () => {
+    return (<><h1>AdminMySpaces</h1></>)
+}
+
+export default AdminMySpaces
