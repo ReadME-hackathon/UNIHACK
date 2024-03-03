dcedecdbeec4e4f36bad2e57130594001dc7d92a
Leo Heng <leoheng905@gmail.com>
2024-03-03 15:21:20 +1100

    Added animations

--- src/components/Landing/LandingFeature.tsx
+++ src/components/Landing/LandingFeature.tsx
@@ -1,13 +1,30 @@
 import LandingFeatureProps from "./LandingFeatureProps";
+import { motion } from "framer-motion";
 
-const LandingFeature = ({ name, desc, img } : LandingFeatureProps) => {
+const LandingFeature = ({ name, desc, img }: LandingFeatureProps) => {
   return (
-    <div className="flex flex-row gap-36 items-center w-[800px] m-auto">
+    <div className="m-auto flex w-[700px] flex-row items-center gap-12">
       <div className="flex flex-col gap-6">
-        <h3 className="text-4xl font-bold">{name}</h3>
-        <p className=" w-72  h-24 font-medium">{desc}</p>
+        <motion.h3
+          initial={{ opacity: 0, x: 20 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, ease: "anticipate", type: "tween" }}
+          viewport={{ once: true }}
+          className="text-4xl font-bold"
+        >
+          {name}
+        </motion.h3>
+        <motion.p
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 0.4, duration: 1, ease: "anticipate", type: "tween" }}
+          viewport={{ once: true }}
+          className="w-fill h-24 font-medium"
+        >
+          {desc}
+        </motion.p>
       </div>
-      <img src={img} className="w-96 h-96"/>
+      <img src={img} className="h-96 w-96" />
     </div>
   );
 };
--- src/routes/landing/LandingPage.tsx
+++ src/routes/landing/LandingPage.tsx
@@ -3,6 +3,7 @@ import LandingBar from "@/components/Landing/LandingBar";
 import LandingFeature from "@/components/Landing/LandingFeature";
 import LandingFeatureProps from "@/components/Landing/LandingFeatureProps";
 import Footer from "@/layouts/Footer/Footer";
+import { motion } from "framer-motion";
 
 const LandingPage = () => {
   const features: LandingFeatureProps[] = [
@@ -24,19 +25,37 @@ const LandingPage = () => {
   ];
 
   return (
-    <div className="max-w-screen-lg m-auto mb-10">
+    <div className="m-auto mb-10 max-w-screen-lg">
       <HeroBanner />
-      <div className="flex flex-col gap-10 w-[900px] rounded-[6px] pt-8 pb-32 mt-14 mx-auto bg-neutral-200">
+      <div className="mx-auto mt-14 flex w-[900px] flex-col gap-10 rounded-[6px] bg-neutral-200 pb-32 pt-8">
         <LandingBar />
-        <div className="flex flex-col items-center gap-6 mt-4">
-          <h2 className="text-6xl font-bold text-center">Less Stressing, More Working</h2>
-          <p className="w-[350px] h-24 mb-10 font-medium text-center">Getting into groups shouldn’t take as much effort as it usually does. Luckily, GroupSpace is here to help you simplify the process and create a better experience!</p>
+        <div className="mt-4 flex flex-col items-center gap-8">
+          <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "anticipate", type: "tween" }}
+            viewport={{ once: true }}
+            className="max-w-md text-center text-6xl font-bold"
+          >
+            Less <span className=" text-orange-500">Stressing</span>, More{" "}
+            <span className="text-indigo-500">Working</span>
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1, ease: "anticipate", type: "tween" }}
+            viewport={{ once: true }}
+            className="mb-10 h-24 max-w-lg text-center font-medium"
+          >
+            Getting into groups shouldn’t take as much effort as it usually does. Luckily,
+            GroupSpace is here to help you simplify the process and create a better experience!
+          </motion.p>
         </div>
         <div className="grid gap-24 ">
-              {features.map((feature) => (
-                <LandingFeature {...feature} />
-              ))}
-            </div>
+          {features.map((feature) => (
+            <LandingFeature {...feature} />
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
