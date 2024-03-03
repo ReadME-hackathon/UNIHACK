10dc69a6ae6fd47673b89d04c448d4516a63c3d8
Leo Heng <leoheng905@gmail.com>
2024-03-03 14:32:59 +1100

    TS imports

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -3,23 +3,27 @@ import { Link } from "react-router-dom";
 import { URLs } from "@/main.tsx";
 
 import opendoor from "@/assets/images/opendoor.png";
-import highfive from "@/assets/images/highfive.png";
-import constellation from "@/assets/images/constellation.png";
+// import highfive from "@/assets/images/highfive.png";
+// import constellation from "@/assets/images/constellation.png";
 import highfive2 from "@/assets/images/highfive2.png";
 import constellation2 from "@/assets/images/constellation2.png";
 
 const HeroBanner = () => {
   return (
-
     <div className="grid grid-cols-[repeat(20,minmax(50px,50px))] grid-rows-[repeat(12,minmax(50px,50px))]">
-      <img className=" col-start-1 col-end-6 row-start-2 row-end-7 rounded-[4px] "
-      src={highfive2} ></img>
-      <img className=" col-start-1 col-end-10 row-start-9 row-end-13 rounded-[4px] "
-      src={constellation2}
+      <img
+        className=" col-start-1 col-end-6 row-start-2 row-end-7 rounded-[4px] "
+        src={highfive2}
       ></img>
-      <img className=" col-start-[17] col-end-[26] row-start-4 row-end-15 rounded-[4px] "
-       src={opendoor} alt="Image">
-      </img>
+      <img
+        className=" col-start-1 col-end-10 row-start-9 row-end-13 rounded-[4px] "
+        src={constellation2}
+      ></img>
+      <img
+        className=" row-end-15 col-start-[17] col-end-[26] row-start-4 rounded-[4px] "
+        src={opendoor}
+        alt="Image"
+      ></img>
       <h2 className="col-start-4 row-start-5 w-[750px] text-center text-6xl font-bold">
         A new way to find teammates
       </h2>
