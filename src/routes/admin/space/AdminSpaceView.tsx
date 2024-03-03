daf6bbeb520ebd0f85ecf4e0a568be116c80ae58
Leo Heng <leoheng905@gmail.com>
2024-03-03 14:04:30 +1100

    Changes

--- src/routes/admin/space/AdminSpaceView.tsx
+++ src/routes/admin/space/AdminSpaceView.tsx
@@ -13,9 +13,9 @@ const AdminSpaceView = () => {
   const [spaceName, setSpaceName] = useState("");
 
   const copyLink = () => {
-    navigator.clipboard.writeText(window.location.href)
-    alert("Link copied!")
-  }
+    navigator.clipboard.writeText(window.location.href);
+    alert("Link copied!");
+  };
 
   // Gets the space ID
   let { space_id } = useParams();
@@ -39,44 +39,45 @@ const AdminSpaceView = () => {
 
   return (
     <>
-    <div className="flex w-full flex-row h-screen px-8 py-3">
-      <div className="w-2/3 pr-8">
-        <div className="flex flex-col gap-2">
-          <h3 className="text-lg font-bold">{`Code: ${space_id}`}</h3>
-          <h1 className="text-6xl font-bold">{spaceName}</h1>
-          <div className="flex items-center gap-7 my-6">
-            <h2 className="text-3xl font-bold ">Groups</h2>
+      <div className="flex h-screen w-full flex-row px-8 py-3">
+        <div className="w-2/3 pr-8">
+          <div className="flex flex-col gap-2">
+            <h3 className="text-lg font-bold">{`Code: ${space_id}`}</h3>
+            <h1 className="text-6xl font-bold">{spaceName}</h1>
+            <div className="my-6 flex items-center gap-7">
+              <h2 className="text-3xl font-bold ">Groups</h2>
+            </div>
           </div>
-        </div>
-        <div className="3xl:grid-cols-4 grid grid-flow-row grid-cols-2 gap-4 2xl:grid-cols-3 overflow-y-auto">
-          {spaceData?.subcollections.groups
-            ? spaceData?.subcollections.groups.map((team: any, index) => (
+          <div className="3xl:grid-cols-4 grid grid-flow-row grid-cols-2 gap-4 overflow-y-auto 2xl:grid-cols-3">
+            {spaceData?.subcollections.groups ? (
+              spaceData?.subcollections.groups.map((team: any, index) => (
                 <TutorialCard
-                  name={team.name}
-                  enrolled={team.member_count}
+                  name={team.data.name}
+                  enrolled={team.data.member_count}
                   size={spaceData?.space.max_size || 0}
-                  description={team.description}
+                  description={team.data.description}
                   key={index}
+                  id={team.id}
                 />
               ))
-            : <></>}
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+        <div className="flex max-h-[950px] min-h-[700px] w-1/3 flex-col justify-between gap-10">
+          <MembersList spaceUser={[]}></MembersList>
+          <div
+            onClick={copyLink}
+            className="mb-16 flex h-1/6 items-center justify-center gap-5 bg-neutral-100 text-3xl font-bold underline hover:cursor-pointer"
+          >
+            <Link2Icon className="mt-2 scale-150" />
+            Share Link
+          </div>
         </div>
       </div>
-      <div className="w-1/3 flex flex-col justify-between gap-10 max-h-[950px] min-h-[700px]">
-        <MembersList spaceUser={[]}></MembersList>
-        <div onClick={copyLink} className="hover:cursor-pointer h-1/6 bg-neutral-100 mb-16 font-bold underline flex gap-5 items-center justify-center text-3xl"><Link2Icon className="scale-150 mt-2"/>Share Link</div>
-      </div>
-    </div>
     </>
   );
 };
 
 export default AdminSpaceView;
-
-
-
-
-
-
-
-
