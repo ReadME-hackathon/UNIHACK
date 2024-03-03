a04ebea3cec67fcb4fddb522d7642a1fac4c3d71
Leo Heng <leoheng905@gmail.com>
2024-03-03 15:44:51 +1100

    Loading team changes

--- src/routes/loading/LoadingTeamPage.tsx
+++ src/routes/loading/LoadingTeamPage.tsx
@@ -1,20 +1,13 @@
 import LoadingTeam from "@/components/Loading/LoadingTeam";
-<<<<<<< HEAD
 import "./LoadingTeamPage.css";
-import planet from "@/assets/images/planet.png"
+import planet from "@/assets/images/planet.png";
 
 const LoadingTeamPage = () => {
   return (
-    <img className="w-screen h-screen pt-[15%] fixed bg-slate-200 "src={planet} >
-=======
-
-const LoadingTeamPage = () => {
-  return (
-    <div className="w-screen h-screen pt-[10vh] fixed bg-slate-200">
->>>>>>> 10dc69a6ae6fd47673b89d04c448d4516a63c3d8
+    <img className="fixed h-screen w-screen bg-slate-200 pt-[15%] " src={planet}>
       <LoadingTeam />
     </img>
-  )
-}
+  );
+};
 
 export default LoadingTeamPage;
