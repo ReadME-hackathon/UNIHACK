8833e22bb0414c52e1549c39ccf609c693a88b64
ttgttg <alicejiayihu@hotmail.com>
2024-03-03 15:15:16 +1100

    uploaded all illust files and updated SignIn UI

--- src/routes/loading/LoadingTeamPage.tsx
+++ src/routes/loading/LoadingTeamPage.tsx
@@ -1,11 +1,12 @@
 import LoadingTeam from "@/components/Loading/LoadingTeam";
 import "./LoadingTeamPage.css";
+import planet from "@/assets/images/planet.png"
 
 const LoadingTeamPage = () => {
   return (
-    <div className="w-screen h-screen pt-[15%] fixed bg-slate-200">
+    <img className="w-screen h-screen pt-[15%] fixed bg-slate-200 "src={planet} >
       <LoadingTeam />
-    </div>
+    </img>
   )
 }
 
--- src/routes/signIn/SignIn.tsx
+++ src/routes/signIn/SignIn.tsx
@@ -1,6 +1,8 @@
 import { signInWithGoogle } from "@/services/firestoreServices";
 import { useNavigate } from "react-router-dom";
 import "./SignIn.css";
+import welcome from "@/assets/images/welcome.png"
+import logo from "@/assets/images/logo.png"
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -15,13 +17,14 @@ const SignIn = () => {
   };
 
   return (
-    <div className="flex h-screen w-screen max-w-full flex-row">
+    <div className="flex h-screen w-screen max-w-full flex-row bg-indigo-800">
       <div className=" h-full w-3/5 p-16">
-        <img className="h-full w-full rounded"></img>
+        <img className="h-full w-full rounded object-contain"src={welcome}></img>
       </div>
       <div className="flex h-full w-2/5 items-center">
         <div className="flex flex-col gap-4 rounded bg-neutral-50 p-8">
-          <img className=" h-80 w-80"></img>
+        <p className="text-4xl font-bold text-center">GroupSpace</p>
+          <img className=" h-40 w-40"src={logo}></img>
           <button onClick={signIn} className="login-with-google-btn transition-all hover:shadow-md">
             Sign in
           </button>
